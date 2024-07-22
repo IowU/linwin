ca05bad61a633621374bc1d382d2544370b976aa
Andrea Mucciarelli <andrea92.pinco@gmail.com>
2024-07-22 23:46:09 +0200

    First attempt to refactor the cat command with the cobra framework

---
+++ cmd/root.go
@@ -0,0 +1,18 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "linwin",
+	Short: "linwin is my porting of some base linux commands to windows",
+	Long: `Implementation is a work in progress. Don't use it in production environments
+                or other critical environments.`,
+}
+
+func Execute() {
+	rootCmd.AddCommand(cmdCat)
+	rootCmd.Execute()
+
+}
